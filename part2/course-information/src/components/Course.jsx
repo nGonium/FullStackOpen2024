3ab9512861aedf61bada5ae3d75028405cc76e8c
Didier van Os <dvos@sogyo.nl>
2023-10-10 09:38:03 +0200

    Extract Course

---
+++ part2/course-information/src/components/Course.jsx
@@ -0,0 +1,13 @@
+import Content from "./Content"
+import Header from "./Header"
+import Total from "./Total"
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+)
+
+export default Course
