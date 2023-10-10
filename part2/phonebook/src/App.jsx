a0f5a38b92e8ee9782e2af1d2c453d7aacfe9eeb
Didier van Os <dvos@sogyo.nl>
2023-10-10 16:18:43 +0200

    Filter list by partial name match

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  const [nameFilter, setNameFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -19,15 +20,25 @@ const App = () => {
     setNewName('')
   }
 
+  const handleNameFilterChange = (event) => 
+    setNameFilter(event.target.value)
+
   const handleNameChange = (event) => 
     setNewName(event.target.value)
 
   const handleNumberChange = (event) => 
     setNewNumber(event.target.value)
 
+  const lowerCaseNameFilter = nameFilter.toLowerCase()
+  const personsToShow = nameFilter 
+    ? persons.filter(({ name }) => name.toLowerCase().includes(lowerCaseNameFilter))
+    : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter shown with <input value={nameFilter} onChange={handleNameFilterChange} /></p>
+      <h2>Add a new</h2>
       <form onSubmit={handleAddNameSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -40,7 +51,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name, number }) => <p key={name}>{name} {number}</p>)}
+      {personsToShow.map(({ name, number }) => <p key={name}>{name} {number}</p>)}
     </div>
   )
 }
